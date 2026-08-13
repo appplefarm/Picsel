53ed91a87fb24481bd538c5cb526968eef56862c
Nayoung.Kim <125175518+na0k1m@users.noreply.github.com>
2026-08-13 20:57:10 +0900

    프로젝트 초기세팅

---
+++ Picxel/Sources/SwiftUIViewExample.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIViewExample.swift
+//  Picxel
+//
+//  Created by 김나영 on 8/13/26.
+//
+
+import SwiftUI
+
+struct SwiftUIViewExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIViewExample()
+}
